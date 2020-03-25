44782aeb7669b3b5e8dcac6c5c8b93e74fc592f3
JustinPooters <48656828+JustinPooters@users.noreply.github.com>
2020-03-25 18:46:24 +0100

    Created the bot
    Bot Release 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Basic DiscordJS bot
  DiscordJS bot basics.
+
+# Made by?
+  This bot is made by Justin, A.k.a EncryptedWeb
+  I've made bots for about 1 year now and i'm sharing a basic code example with you guys.
+
+# Setup tutorial
+  Go to the terminal and typ the following commands
+  ```
+    npm init
+  ```
+  and after that
+  ```  
+    npm install discord.js
+  ```
+  go to https://discordapp.com/developers
+  and create a bot
+  get the "token" and replace the "TOKEN_HERE" with your token.
+  to start the bot go to your terminal and typ: ``node index.js``
+
+# Free to change
+  Change everything you want, just download the source, and add commands
+
+# Example commands for support?
+  I've included a "BASIC COMMAND.txt" File to help you make new commands
+
+# Have fun with it!
+
+-  Justin
---
+++ index.js
@@ -0,0 +1,86 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+let config = require("config.json")
+
+
+client.on("ready", () => {
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  client.user.setActivity(`BasicBot | EncryptedWeb.site`);
+});
+
+
+client.on("message", async message => {
+
+  if(message.author.bot) return;
+
+  if(message.content.toLowerCase().includes("bot")) {
+    message.reply("**Hi! I'm here!**")
+  }
+  
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  if(command === "help") {
+    let helpembed = new Discord.RichEmbed()
+    .setTitle("HelpMenu")
+    .setDescription("This are all command")
+    .addField("say", "Says the command")
+    .addField("kick", "Kicks a user")
+    .addField("ban", "Bans a user")
+    .addField("purge", "Purges a ammount of messages")
+    .addField("ping", "Checks API Latency")
+  }
+  
+  
+  if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+    if(!message.author.hasPermission("KICK_MEMBERS")) return message.reply("Sorry, you don't have permissions to use this!");
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";  
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+  
+  if(command === "ban") {
+    if(!message.author.hasPermission("BAN_MEMBERS")) return message.reply("Sorry, you don't have permissions to use this!");
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+  
+  if(command === "purge") {
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+});
+
+client.login(config.token);
